2216374399f36e3b57465c6fe070c1d0425e39fe
Marcel Cruz <marcelcruz@live.com>
2019-08-17 15:26:06 +0200

    🔨 Add getWinner and createMessage call to tweet.js

--- tweet.js
+++ tweet.js
@@ -1,38 +1,44 @@
 const OAuth = require('oauth')
+const createMessage = require('./create-message')
+const getWinner = require('./get-winner')
 
-const application_consumer_key = process.env.APPLICATION_CONSUMER_KEY
-const application_secret = process.env.APPLICATION_SECRET
-const user_access_token = process.env.USER_ACCESS_TOKEN
-const user_secret = process.env.USER_SECRET
+module.exports = async period => {
+  const applicationConsumerKey = process.env.APPLICATION_CONSUMER_KEY
+  const applicationSecret = process.env.APPLICATION_SECRET
+  const userAccessToken = process.env.USER_ACCESS_TOKEN
+  const userSecret = process.env.USER_SECRET
 
-const oauth = new OAuth.OAuth(
-  'https://api.twitter.com/oauth/request_token',
-  'https://api.twitter.com/oauth/access_token',
-  application_consumer_key,
-  application_secret,
-  '1.0A',
-  null,
-  'HMAC-SHA1',
-)
+  try {
+    const winner = await getWinner(period)
+    const message = createMessage(winner, period)
 
-const status = 'This is our first tweet!'
+    const oauth = new OAuth.OAuth(
+      'https://api.twitter.com/oauth/request_token',
+      'https://api.twitter.com/oauth/access_token',
+      applicationConsumerKey,
+      applicationSecret,
+      '1.0A',
+      null,
+      'HMAC-SHA1',
+    )
 
-const postBody = {
-  status,
-}
-
-oauth.post(
-  'https://api.twitter.com/1.1/statuses/update.json',
-  user_access_token,
-  user_secret,
-  postBody,
-  '',
-
-  function(err, data) {
-    if (err) {
-      console.log('Error: ', err)
-    } else {
-      console.log('Data: ', data)
+    const postBody = {
+      status: message,
     }
-  },
-)
+
+    oauth.post(
+      'https://api.twitter.com/1.1/statuses/update.json',
+      userAccessToken,
+      userSecret,
+      postBody,
+      '',
+      error => {
+        if (error) {
+          console.log('Error tweeting: ', error)
+        }
+      },
+    )
+  } catch (error) {
+    console.log(`Error fetching ${period} trending repository: ${error}`)
+  }
+}
